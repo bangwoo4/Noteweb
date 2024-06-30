126a1362a4978c687c7958c018d72957e62b4206
Bằng <133222169+bangwoo4@users.noreply.github.com>
2024-06-30 12:47:17 +0700

    file error fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,60 @@
 import "./App.css";
 import React from "react";
-import Header from "./components/Header";
-import MediaPlayer from "./components/MediaPlayer";
+import Login from "./components/Login";
+import NotesSelector from "./components/NotesSelector";
+import DisplayNotes from "./components/DisplayNotes";
+import Headers from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
+  //login
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+
+  const [selectedNote, setSelectedNote] = React.useState(null);
+  const [noteBody, setNoteBody] = React.useState("");
+
+  const handleNoteSelect = (note) => {
+    setSelectedNote(note);
+    setNoteBody(note.body);
+  };
+
+  const handleNoteBodyChange = (event) => {
+    setNoteBody(event.target.value);
+  };
+
+  const handleNoteSave = (note, body) => {
+    const updatedNote = { ...note, body };
+    setSelectedNote(updatedNote);
+  };
+
+  const handleNoteUpdate = (updatedNote) => {
+    setSelectedNote(updatedNote);
+  };
+
   return (
     <div className="App">
-      <div className="App-header">
-        <Header />
+      <div className="header">
+        <Headers />
       </div>
-      <div className="App-media-player">
-        <MediaPlayer />
-      </div>
-      <div className="App-footer">
+      {!isLoggedIn && <Login onLogin={() => setIsLoggedIn(true)} />}
+      {isLoggedIn && (
+        <div className="content">
+          <NotesSelector
+            onNoteSelect={handleNoteSelect}
+            selectedNote={selectedNote}
+            onNoteUpdate={handleNoteUpdate}
+          />
+          <DisplayNotes
+            selectedNote={selectedNote}
+            noteBody={noteBody}
+            onNoteBodyChange={handleNoteBodyChange}
+            onNoteSave={handleNoteSave}
+            onNoteUpdate={handleNoteUpdate}
+          />
+        </div>
+      )}
+
+      <div className="footer">
         <Footer />
       </div>
     </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
-import React from "react";
-import "./Footer.css";
+import React, { useState, useEffect, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFacebook,
@@ -8,11 +7,31 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
+  const [showContactModal, setShowContactModal] = useState(false);
+  const modalRef = useRef();
+
+  const toggleContactModal = () => {
+    setShowContactModal(!showContactModal);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        setShowContactModal(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <footer className="App-footer">
       <div className="footer-content">
-        <p>bangwoo4</p>
-        <p>&copy; Copyright 2024 - React Media Player </p>
+        <p>&copy; 2024 Note Taking App. All rights reserved.</p>
+        <p>Designed by Bang - CSE - HCMUT</p>
         <nav>
           <ul>
             <li>
@@ -43,6 +62,30 @@ function Footer() {
               <a href="https://github.com/bangwoo4" aria-label="Github">
                 <FontAwesomeIcon icon={faGithub} /> Github
               </a>
+            </li>
+            <li>
+              <button
+                className="contactButton"
+                onClick={toggleContactModal}
+                aria-label="Contact Us"
+              >
+                About
+              </button>
+              {showContactModal && (
+                <div className="contact-modal" ref={modalRef}>
+                  <h3>Contact</h3>
+                  <p>Nguyen Dinh Bang - 2210298</p>
+                  <p>nguyendinhbang53az@gmail.com</p>
+                  <p>bang.nguyendinh@hcmut.edu.vn</p>
+                  <p>0901236608</p>
+                  <button
+                    onClick={toggleContactModal}
+                    aria-label="Close Contact Modal"
+                  >
+                    Close
+                  </button>
+                </div>
+              )}
             </li>
           </ul>
         </nav>
